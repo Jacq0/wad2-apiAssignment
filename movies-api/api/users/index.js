b9e7acbeb97545a7540c64b77b5a0922e111c5f0
Jacq0 <59923922+Jacq0@users.noreply.github.com>
2021-12-30 22:36:09 +0000

    Mongo lab error handling

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,5 +1,7 @@
+import asyncHandler from 'express-async-handler';
 import express from 'express';
 import User from './userModel';
+
 
 const router = express.Router(); // eslint-disable-line
 
@@ -10,23 +12,24 @@ router.get('/', async (req, res) => {
 });
 
   // register(Create)/Authenticate User
-  router.post('/', async (req, res) => {
+ // register
+ router.post('/', asyncHandler(async (req, res) => {
     if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
+        await User(req.body).save()
         res.status(201).json({
             code: 201,
             msg: 'Successful created new user.',
         });
     }
-    else {  //Must be an authenticate then!!! Query the DB and check if there's a match
+    else {  //Must be authenticating the!!! Query the DB and check if there's a match
         const user = await User.findOne(req.body);
         if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-        }else{
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
         }
     }
-});
+}));
 
   // Update a user
   router.put('/:id', async (req, res) => {
@@ -40,4 +43,28 @@ router.get('/', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
 });
+
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
+        }
+    }
+});
+
+router.get('/:id/favourites', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.status(200).json(user.favourites);
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
+    }
+});
+
 export default router;
