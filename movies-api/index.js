4bad845a018dc5d8ee78773ad49e4952044b89b0
Jacq0 <59923922+Jacq0@users.noreply.github.com>
2021-12-30 22:09:07 +0000

    Mongo lab users router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -14,8 +15,12 @@ const port = process.env.PORT;
 //middleware stack!
 app.use(express.json());
 
+//movie and genre routers
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
+
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
